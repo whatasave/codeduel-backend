4881d0980e915e0ebbb8e1917fddb65b002b4cf4
Michi Palazzo <palazzomichi@gmail.com>
2024-05-31 01:08:06 +0200

    Update

--- src/User/Data.cs
+++ src/User/Data.cs
@@ -4,11 +4,12 @@ public record User(
     int Id,
     string Username,
     string Name,
+    DateTime CreatedAt,
     string? Avatar = null,
     string? BackgroundImage = null,
     string? Biography = null
 ) {
-    public User(Entity user) : this(user.Id, user.Username, user.Name, user.Avatar, user.BackgroundImage, user.Biography) { }
+    public User(Entity user) : this(user.Id, user.Username, user.Name, user.CreatedAt, user.Avatar, user.BackgroundImage, user.Biography) { }
 }
 
 public record UserListItem(
