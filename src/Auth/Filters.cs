26f8b8bf1b3ed10a72f99a430d335fb1d02ec77b
Michi Palazzo <palazzomichi@gmail.com>
2024-07-07 23:15:58 +0200

    Fix

--- src/Auth/Filters.cs
+++ src/Auth/Filters.cs
@@ -12,48 +12,57 @@ public class OptionalAuthAttribute : Attribute;
 public class InternalAuthAttribute : Attribute;
 
 public class Middleware(Config.Config config, Service service) {
-    public Task Auth(HttpContext context, Func<Task> next) {
+    public async Task Auth(HttpContext context, Func<Task> next) {
         var hasAttribute = context.Features.Get<IEndpointFeature>()?.Endpoint?.Metadata
                 .Any(m => m is AuthAttribute) ?? false;
-        if (!hasAttribute) return next();
+        if (!hasAttribute) {
+            await next();
+            return;
+        }
 
         context.Request.Cookies.TryGetValue(config.Auth.AccessTokenCookieName, out var accessToken);
         if (accessToken == null) {
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            context.Response.WriteAsync("Unauthorized");
-            return Task.CompletedTask;
+            await context.Response.WriteAsync("Unauthorized");
+            return;
         }
 
         var payload = service.ValidateAccessToken(accessToken);
         context.Items["auth"] = payload;
-        return next();
+        await next();
     }
 
-    public Task OptionalAuth(HttpContext context, Func<Task> next) {
+    public async Task OptionalAuth(HttpContext context, Func<Task> next) {
         var hasAttribute = context.Features.Get<IEndpointFeature>()?.Endpoint?.Metadata
                 .Any(m => m is OptionalAuthAttribute) ?? false;
-        if (!hasAttribute) return next();
+        if (!hasAttribute) {
+            await next();
+            return;
+        }
 
         context.Request.Cookies.TryGetValue(config.Auth.AccessTokenCookieName, out var accessToken);
         if (accessToken != null) {
             var payload = service.ValidateAccessToken(accessToken);
             context.Items["auth"] = payload;
         }
-        return next();
+        await next();
     }
 
-    public Task InternalAuth(HttpContext context, Func<Task> next) {
+    public async Task InternalAuth(HttpContext context, Func<Task> next) {
         var hasAttribute = context.Features.Get<IEndpointFeature>()?.Endpoint?.Metadata
                 .Any(m => m is InternalAuthAttribute) ?? false;
-        if (!hasAttribute) return next();
+        if (!hasAttribute) {
+            await next();
+            return;
+        }
 
         context.Request.Headers.TryGetValue(config.Auth.ServiceHeaderName, out var serviceToken);
         if (serviceToken.All(token => token != config.Auth.ServiceToken)) {
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            context.Response.WriteAsync("Unauthorized");
-            return Task.CompletedTask;
+            await context.Response.WriteAsync("Unauthorized");
+            return;
         }
-        return next();
+        await next();
     }
 }
 public static class Extensions {
